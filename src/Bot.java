05d8efc74c93969b4fa87ff2d055ac07de0c6f69
kowlad <wladickowalew@gmail.com>
2020-04-15 17:46:40 +0300

    first commit

---
+++ src/Bot.java
@@ -0,0 +1,54 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi botapi = new TelegramBotsApi();
+        try {
+            botapi.registerBot(new Bot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        String text = message.getText();
+        System.out.println(text);
+        if (text.equals("ping")){
+            sendMessage(message, "pong");
+        }else{
+            sendMessage(message, "it's not ping");
+        }
+    }
+
+    private void sendMessage(Message m, String text){
+        SendMessage message = new SendMessage();
+        message.setChatId(m.getChatId());
+        message.setText(text);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "java_try_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "1270371974:AAG3thXALtUx_pzISpRnq1Hz2F8Nn3MkeIA";
+    }
+}
